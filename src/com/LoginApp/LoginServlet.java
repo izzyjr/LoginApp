3345e1610b2a2e3c9f8bca87e89d39b9a8d845b7
Israel Mesa <mesa.israel50@gmail.com>
2018-10-04 20:20:51 -0400

    controller servlet

---
+++ src/com/LoginApp/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.LoginApp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.LoginApp.service.LoginService;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userId, password;
+		
+		userId = request.getParameter("userId");
+		password = request.getParameter("password");
+		
+		LoginService loginService = new LoginService();
+		boolean results = loginService.authenticate(userId, password);
+		
+	}
+
+}
